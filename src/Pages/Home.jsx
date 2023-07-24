31d1fe2004e6eed16427c6b60f73f46333865759
Amy Saeed <ammythecreator@gmail.com>
2023-07-24 03:53:36 -0700

    added a form and made created a callback onSubmit that holds fetch and returns data from MAZE TV API dynamically by replacing the endpoint with strSearch so now we can get data bcak from the API when we search in the input area

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,25 @@
+import { useState } from 'react';
 const Home = () => {
-  return <div>Home</div>;
+  const [searchStr, setSearchStr] = useState('');
+  const onSearchInputChange = e => {
+    setSearchStr(e.target.value);
+  };
+
+  const onSearch = async e => {
+    e.preventDefault();
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = await response.json();
+    console.log(body);
+  };
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" onChange={onSearchInputChange} value={searchStr} />
+        <button type="submit">Update </button>
+      </form>
+    </div>
+  );
 };
 export default Home;
