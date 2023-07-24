b6a28c5defe69020271a969ff47b108bf3c0bb66
Amy Saeed <ammythecreator@gmail.com>
2023-07-24 06:24:22 -0700

    we refactored the code by moving the function responsible for fetchig to its own component we then made the code cleaner by creating a base-link with the part of the api url that we know wont change we gave the fucntion getapi an argument which will be the part of the api we will modify inside the fucntion we returned the a string template with the base as the first var and the arg from the function as the second var and then we craeted anotehr fucntion that calls getapi and takes an arg that will be our strinput state and we passed it as the arg for getapi then we exported it then we imported it to home and then we created another state called apidata and based the result of the fetch fucntion to it after calling it inside onsearch fucntion and then we created anotehr fucn to render the infromation coming from the api and we used map on it and inside we rendered a div with the name propeerty from the apidata obj and then we worked on hadling possible error from the api by craeting a catch and try block and then craetng another state to hold the error and to set the erro and passed the result of the catch block to the erro state and then went to the render fucntion and careted a conditional based on whether there is an error inside the error state as we gave it an inital value of null if there is an error it'll render a div with the value of error happened and then we used the erro.message to get the specific error that happened

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,24 +1,44 @@
 import { useState } from 'react';
+import { searchForShows } from '../api/tvmaze';
 const Home = () => {
   const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
+  // console.log(searchStr, apiData, apiDataError);
   const onSearchInputChange = e => {
     setSearchStr(e.target.value);
   };
 
   const onSearch = async e => {
     e.preventDefault();
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q=${searchStr}`
-    );
-    const body = await response.json();
-    console.log(body);
+
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+      console.log(error);
+    }
   };
+
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div> Error Occured: {apiDataError.message}</div>;
+    }
+    if (apiData) {
+      apiData.map(data => <div key={data.show.id}> {data.show.name} </div>);
+    }
+    return null;
+  };
+
   return (
     <div>
       <form onSubmit={onSearch}>
         <input type="text" onChange={onSearchInputChange} value={searchStr} />
         <button type="submit">Update </button>
       </form>
+      <div>{renderApiData()}</div>
     </div>
   );
 };
---
+++ src/api/tvmaze.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://api.tvmaze.com';
+const getApi = async queryString => {
+  const response = await fetch(`${BASE_URL}${queryString}`);
+  const body = await response.json();
+  return body;
+};
+
+export const searchForShows = q => getApi(`/search/shows?q=${q}`);
